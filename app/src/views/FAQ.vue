52ca4ba5f5a10477d6daf265617845c6aee8802e
Luke Massa <lmassa@tripadvisor.com>
2022-02-20 23:04:55 -0500

    Fix typo

--- app/src/views/FAQ.vue
+++ app/src/views/FAQ.vue
@@ -3,7 +3,7 @@
     <ul>
       <li><b>What Pandemic Precautions Will Be In Place?</b><br>We ask that all guests be fully vaccinated against COVID-19. We will also likely have a testing requirement, and will clarify as the date gets closer.</li>
       <br>
-      <li><b>Are Children Invited?</b><br>We're lucky to have so many growing families among our nearest and dearest — but unless we've spoken to you directly, only adult are being invited to the event.<br>Please do not hesitate to let us know if this will present logistic challenges, and how we might be able to help.</li>
+      <li><b>Are Children Invited?</b><br>We're lucky to have so many growing families among our nearest and dearest — but unless we've spoken to you directly, only adults are being invited to the event.<br>Please do not hesitate to let us know if this will present logistic challenges, and how we might be able to help.</li>
 
     </ul>
   </div>
