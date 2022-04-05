5f8997af6aa33ee517938de483b369b0dc54ba80
Luke Massa <lmassa@tripadvisor.com>
2022-04-04 21:29:31 -0400

    Update travel

--- app/src/views/Schedule.vue
+++ app/src/views/Schedule.vue
@@ -5,6 +5,8 @@
       <li><b>Friday, September 30th</b><br>Welcome Cocktails at <a href="https://www.charleshotel.com/dining/regattabar" target="_blank">The Regatta Bar at The Charles Hotel</a> in Cambridge, MA</li>
       <br>
       <li><b>Saturday, October 1st</b><br>Wedding, Cocktails, and Reception at <a href="https://www.commandersmansion.com/" target="_blank">The Commander's Mansion</a> in Watertown, MA</li>
+      <br>
+      <li><b>Sunday, October 2nd</b><br>Farewell Coffee and Bagels at our apartment in <a href="https://www.google.com/maps/place/Inman+Square,+Cambridge,+MA/@42.3730489,-71.1002989,17z" target="_blank">Inman Square</a>, Cambridge, MA</li>
 
     </ul>
   </div>
