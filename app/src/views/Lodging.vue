acb4e1b374939f366f8a035979f6767c69b4fb9e
Luke Massa <lmassa@tripadvisor.com>
2022-04-04 21:48:42 -0400

    Update travel

--- app/src/views/Lodging.vue
+++ app/src/views/Lodging.vue
@@ -4,11 +4,11 @@
     <p>We have reserved blocks of rooms at two local hotels: </p>
     <ul>
             <li><strong><a target="_blank" href="https://www.phgsecure.com/IBE/bookingRedirect.ashx?propertyCode=BOSCH&group=shnk0930&arrivalDate=09-30-2022&departureDate=10-02-2022&numberOfNights=2&numberOfAdults=1">The Charles Hotel</a></strong> is about a mile away in lively Harvard Square. It is accessible from the airport on the Red Line on the MBTA.</li>
-            <li><strong><a target="_blank" href="https://www.hilton.com/en/attend-my-event/boswwhx-amb-87679a26-1374-4c3e-8bd0-1cd9a123c754/">Hampton Inn and Suites, Watertown</a></strong> is within walking distance of the venue. It is accessible from the airport via Taxi/Car Share</li>
+            <li><strong><a target="_blank" href="https://www.hilton.com/en/attend-my-event/boswwhx-amb-87679a26-1374-4c3e-8bd0-1cd9a123c754/">Hampton Inn and Suites, Watertown</a></strong> is within walking distance of the venue. It is accessible from the airport via Taxi/Ride Share.</li>
          </ul>
          <p>You are also free to call either hotel directly and ask for the Andersen-Massa Block Rate.</p>
  
-         <p>In general, we recommend getting around the area on public transportation or car share apps. We will have parking at the Commander's Mansion on Saturday, but parking at the Charles Hotel on Friday and our apartment on Sunday, and in general in the Boston area, is very limited.</p>
+         <p>In general, we recommend getting around the area on public transportation or ride share apps. There will be ample parking at the Commander's Mansion on Saturday, but otherwise parking in the Boston area is very limited.</p>
         <br>
   </div>
 </template>
