0c66b13dd16da3e7c9f92fd56bb4a6c8be0a7cef
Luke Massa <lmassa@tripadvisor.com>
2022-08-08 16:44:28 -0400

    Add FAQs

--- app/src/views/FAQ.vue
+++ app/src/views/FAQ.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="faq">
     <ul>
-      <li><b>What Pandemic Precautions Will Be In Place?</b><br>We require that all guests be fully vaccinated against COVID-19, and take an antigen or PCR test within 48 hours of the ceremony.</li>
+      <li><b>What Pandemic Precautions Will Be In Place?</b>
+      <br>We require that all guests be fully vaccinated against COVID-19, and take an antigen or PCR test within 48 hours of the ceremony. Masks are welcome but not required.
+      <p>The wedding ceremony and reception will be outside under a tent. The post-ceremony cocktail hour will be partially inside the mansion, and the after-party and Friday night welcome cocktails will be entirely inside the Charles Hotel.</p>
+      <p>We recognize that conditions may change in the next few months, and will keep our guests up to date via this page as well as email. Feel free to reach out in the interim if you have additional questions.</p>
+      </li>
       <br>
       <li><b>Are Children Invited?</b>
          <br>We're lucky to have so many growing families among our nearest and dearest — but unless we've spoken to you directly, only adults are invited to the event.
